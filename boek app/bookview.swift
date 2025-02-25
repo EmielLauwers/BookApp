1265a26dc9baa8e3c4eb4563c9df58ca4dd7fe4f
EmielLauwers <lauwers.emiel@gmail.com>
2025-02-25 13:50:22 +0100

    first commit

---
+++ boek app/bookview.swift
@@ -0,0 +1,18 @@
+//
+//  bookview.swift
+//  boek app
+//
+//  Created by Emiel lauwers on 25/02/2025.
+//
+
+import SwiftUI
+
+struct bookview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    bookview()
+}
---
+++ boek app/homeView.swift
@@ -0,0 +1,18 @@
+//
+//  homeView.swift
+//  boek app
+//
+//  Created by Emiel lauwers on 25/02/2025.
+//
+
+import SwiftUI
+
+struct homeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    homeView()
+}
