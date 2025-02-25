ec8f11f96f42ec084ac1b00468155542b769e9e0
EmielLauwers <lauwers.emiel@gmail.com>
2025-02-25 11:31:37 +0100

    Initial Commit

---
+++ boek app/boek_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  boek_appApp.swift
+//  boek app
+//
+//  Created by Emiel lauwers on 25/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct boek_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
